fdd4593e4fbf28d21b7a159168f1dab2c23573a2
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-08-02 02:30:18 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,39 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+
+	constructor(props) {
+		super()
+		this.state = {
+			secondsLeft: props.initialCount
+		}
+
+	}
+
+	handleBoom = () => {	
+		let seconds = this.state.secondsLeft
+		setInterval(() => {
+			this.setState({
+				secondsLeft: seconds-1
+			})
+		}, 1000)
+		if (seconds === 0) {
+			return "Boom!"
+		}
+		else {
+			return `${this.state.secondsLeft} seconds left before I go boom!`
+		}
+    }
+    
+
+
+	render() {
+		return (
+			<div>
+			{this.handleBoom()}
+			</div>
+		)
+	}
+
+}
